51c3a32a4518b294d81c80f631a8efc1af62193f
ThomasCode92 <thomascode92@hotmail.com>
2023-02-07 12:32:55 +0100

    __name__

--- packages/main.py
+++ packages/main.py
@@ -6,3 +6,16 @@ print(shopping_cart.buy('apple'))
 
 print(divide(5, 2))
 print(multiply(5, 2))
+
+print(__name__)
+
+if __name__ == '__main__':
+    print('Do something...')
+
+
+class Student():
+    pass
+
+
+student1 = Student()
+print(type(student1))  # => <class '__main__.Student'>
