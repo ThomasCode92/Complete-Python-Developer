c0e44e9dfb940798863869865731fd087f8bb43c
ThomasCode92 <thomascode92@hotmail.com>
2023-02-07 22:55:17 +0100

    Writing Unit Tests

--- test.py
+++ test.py
@@ -3,6 +3,9 @@ import main
 
 
 class TestMain(unittest.TestCase):
+    def setUp(self):
+        print('about to test a function...')
+
     def test_do_stuff(self):
         test_param = 10
         result = main.do_stuff(test_param)
@@ -11,7 +14,21 @@ class TestMain(unittest.TestCase):
     def test_do_stuff2(self):
         test_param = 'abc'
         result = main.do_stuff(test_param)
-        self.assertTrue(isinstance(result, ValueError))
+        self.assertIsInstance(result, ValueError)
+
+    def test_do_stuff3(self):
+        test_param = None
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 'Please enter a number')
+
+    def test_do_stuff4(self):
+        test_param = ''
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 'Please enter a number')
+
+    def tearDown(self):
+        print('cleaning up...')
 
 
-unittest.main()
+if __name__ == '__main__':
+    unittest.main()
