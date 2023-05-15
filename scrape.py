bc41e2cd2beebd5758d65d773647b32ea22e1fed
ThomasCode92 <thomascode92@hotmail.com>
2023-05-15 13:08:00 +0200

    BeautifulSoup Basics

--- scrape.py
+++ scrape.py
@@ -2,4 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/')
-print(res.text)
+soup = BeautifulSoup(res.text, 'html.parser')
+
+print(soup.title)
+print(soup.find_all('div'))
