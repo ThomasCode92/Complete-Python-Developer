6ceace7158fa1d42707bc68bd88fce6065b77dc9
ThomasCode92 <thomascode92@hotmail.com>
2023-05-31 13:03:35 +0200

    URL Parameters

--- server.py
+++ server.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template('index.html')
+
+
+@app.route('/blog/<username>/<int:post_id>')
+def blog_post(username=None, post_id=None):
+    return render_template('blog.html', name=username, post_id=post_id)
