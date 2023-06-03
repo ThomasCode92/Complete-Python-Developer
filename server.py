4e3381e15755f911f7467649e5bdf6b64e57973e
ThomasCode92 <thomascode92@hotmail.com>
2023-06-03 15:43:33 +0200

    Building a Portfolio - Part 1

--- server.py
+++ server.py
@@ -4,10 +4,5 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def my_home():
     return render_template('index.html')
-
-
-@app.route('/blog/<username>/<int:post_id>')
-def blog_post(username=None, post_id=None):
-    return render_template('blog.html', name=username, post_id=post_id)
