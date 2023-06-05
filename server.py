02a3933f01682342b64724c59419cb7d99294023
ThomasCode92 <thomascode92@hotmail.com>
2023-06-05 12:58:28 +0200

    Building a Portfolio - Part 6

--- server.py
+++ server.py
@@ -3,6 +3,14 @@ from flask import Flask, redirect, render_template, request
 app = Flask(__name__)
 
 
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{email}, {subject}, {message}')
+
+
 @app.route("/")
 def my_home():
     return render_template('index.html')
@@ -17,7 +25,7 @@ def html_page(page_name):
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
+        write_to_file(data)
 
         return redirect('/thankyou.html')
     else:
